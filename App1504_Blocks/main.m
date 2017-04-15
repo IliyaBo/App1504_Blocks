1e99defd47d47d8a11f41a8284672643a15c6e44
user <iOS-School-1@iMac-user-8.local>
2017-04-15 12:35:14 +0300

    Initial Commit

---
+++ App1504_Blocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  App1504_Blocks
+//
+//  Created by iOS-School-1 on 15.04.17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
